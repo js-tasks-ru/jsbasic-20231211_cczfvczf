ca341602c25eb06b194bef4c4d866982da2fae2f
Aleksandr Kruglov <cczfvczf@bk.ru>
2023-12-17 15:33:51 +0300

    Модуль 1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
   // ваш код...
+  let answer = false;
+  let newStr = str.toLowerCase();
+  if(newStr.indexOf('1xbet') !== -1 || newStr.indexOf('xxx') !== -1) {
+    answer = true;
+  }
+  return answer
 }
