a4a55f60a041fd0778dafb501e523ff746645f15
Aleksandr Kruglov <cczfvczf@bk.ru>
2023-12-28 22:58:36 +0300

    модуль 3 - итог

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -10,26 +10,3 @@ function showSalary(users, age) {
   }
   return newArr.join('\n');
 }
-
-// let user1 = {
-//   balance: "$1,825.65",
-//   picture: "https://placehold.it/32x32",
-//   age: 21,
-//   name: "Golden Branch",
-//   gender: "male",
-//   "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-//   "favouriteFruit": "banana"
-// };
-
-// let user2 = {
-//   "balance": "$1,825.65",
-//   "picture": "https://placehold.it/32x32",
-//   "age": 25,
-//   "name": "Pety",
-//   "gender": "male",
-//   "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-//   "favouriteFruit": "banana"
-// };
-
-// let users = [user1, user2];
-// console.log(showSalary(users, 30))
