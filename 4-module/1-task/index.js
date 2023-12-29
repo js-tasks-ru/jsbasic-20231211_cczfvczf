dcdaf4e17f6318852432ae357c4e4591336cc76e
Aleksandr Kruglov <cczfvczf@bk.ru>
2023-12-29 20:42:31 +0300

    модуль 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
   // ваш код...
+  
+  let ul = document.createElement('ul');
+  let li = "";
+
+  for (let friend of friends) {
+    li += `<li>${friend.firstName} ${friend.lastName}</li>`;
+  }
+
+  ul.innerHTML = li;
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
   // ваш код...
+  let n = table.rows.length;
+
+  for (let i = 0; i < n; i++) {
+    if (table.rows[i].cells[3].getAttribute('data-available') === 'true') {
+      table.rows[i].classList.add('available');
+    } else if (table.rows[i].cells[3].getAttribute('data-available') === 'false') {
+      table.rows[i].classList.add('unavailable');
+    } else {
+      table.rows[i].setAttribute('hidden', 'hidden');
+    }
+
+    if (table.rows[i].cells[2].textContent == 'm') {
+      table.rows[i].classList.add('male');
+    } else if (table.rows[i].cells[2].textContent == 'f') {
+      table.rows[i].classList.add('female');
+    }
+
+    if (table.rows[i].cells[1].textContent < 18) {
+      table.rows[i].style = "text-decoration: line-through";
+    }
+  }
 }
